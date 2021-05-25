0df2e08a5932f5db7881eb949436eed567fd605c
kwang1991 <aosjtnstn7@naver.com>
2021-05-25 11:22:06 +0900

    app.js 기본설정 작업

--- app.js
+++ app.js
@@ -10,12 +10,20 @@ const session = require('express-session');
 dotenv.config();
 
 const app = express();
-app.set('posrt', process.env.PORT || 3000);
-app.set('view engin', 'html');
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+
 nunjucks.configure('views', {
   express : app,
   watch : true,
 });
+
+app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use("/", express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(process.env.COOKIE_SECRET)); // 쿠키설정
 
 // 없는 페이지 처리 미들웨어 (라우터)
 app.use((req, res, next) => {
